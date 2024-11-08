5f468421d25219512374b181d40edd131f8896b0
Plankton <anton.kristiansson@elev.ga.lbs.se>
2024-11-08 13:44:41 +0100

    sidescroller

--- gamejam_spel_grupp4/Assets/2D plattformer Assets/PlayerMovement.cs
+++ gamejam_spel_grupp4/Assets/2D plattformer Assets/PlayerMovement.cs
@@ -58,27 +58,16 @@ public class PlayerMovement : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "NextLevel" && Input.GetKeyDown(KeyCode.E))
+        if (collision.tag == "NextLevel")
         {
 
-            transform.GetChild(1).gameObject.SetActive(true);
+           
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-        if (collision.tag == "PreviousLevel" && Input.GetKeyDown(KeyCode.E))
+        if (collision.tag == "PreviousLevel")
         {
-            transform.GetChild(1).gameObject.SetActive(true);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "NextLevel" || collision.tag == "PreviousLevel")
-        {
-            transform.GetChild(1).gameObject.SetActive(false);
 
-
-        }
-
-
-    }
 }
